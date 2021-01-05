88bbb9093afbd011aaa51a100df6fc39720f59d9
Benjamin DiGregorio <ben@digregor.io>
2021-01-04 23:30:44 -0800

    throttle double-jumps, add obstacle and scroll environment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRigidbody;
+    
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    private void Start() {
+        playerRigidbody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        } 
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        isOnGround = true;
     }
 }
