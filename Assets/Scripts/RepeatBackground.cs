e3e91a8618b629e2a0a783ce463acf3ee0e0b654
Benjamin DiGregorio <ben@digregor.io>
2021-01-05 23:27:50 -0800

    Calculate repeatWidth properly for background scrolling

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPosition;
+    private float repeatWidth; 
 
     void Start()
     {
         startPosition = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     void Update()    
     {
-        if (transform.position.x < startPosition.x - 50) 
+        if (transform.position.x < startPosition.x - repeatWidth) 
         {
             transform.position = startPosition;
         }   
