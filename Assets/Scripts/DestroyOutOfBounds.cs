7d9c47b556356529f4969ab56983ff39b302ca5d
Benjamin DiGregorio <ben@digregor.io>
2021-01-05 22:23:37 -0800

    Add spawn manager and destroy scripts

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour {
+    
+    private float minXBounds = -5;
+
+    private void Update() {
+        if (transform.position.x < minXBounds) {
+            Destroy(gameObject);
+        }
+    }
+}
