30cc852205cacd84b5f544793d4465fbe63de8ee
Benjamin DiGregorio <ben@digregor.io>
2021-01-09 15:02:02 -0800

    Balloon floats up while spacebar is pressed

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -8,7 +8,7 @@ public class PlayerControllerX : MonoBehaviour
 
     public float floatForce;
     private float gravityModifier = 1.5f;
-    private Rigidbody playerRb;
+    private Rigidbody playerRigidbody;
 
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
@@ -23,9 +23,10 @@ public class PlayerControllerX : MonoBehaviour
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
+        playerRigidbody = GetComponent<Rigidbody>();
 
         // Apply a small upward force at the start of the game
-        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        playerRigidbody.AddForce(Vector3.up * 5, ForceMode.Impulse);
 
     }
 
@@ -35,7 +36,7 @@ public class PlayerControllerX : MonoBehaviour
         // While space is pressed and player is low enough, float up
         if (Input.GetKey(KeyCode.Space) && !gameOver)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            playerRigidbody.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
         }
     }
 
